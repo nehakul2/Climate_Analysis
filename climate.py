b7658008e80ea621e718d97885f8f993b863e57b
NehaKulkarni <nehakulkarni2@.com>
2018-02-10 18:29:51 -0800

    final submission after removing the extra print statements

--- climate.py
+++ climate.py
@@ -1,40 +1,11 @@
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, MetaData
-
 from flask import Flask, jsonify
-
-import climate_analysis.py
-#################################################
-# Database Setup
-#################################################
-engine = create_engine("sqlite:///hawaii.sqlite")
-print("Connected to DB")
-
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(engine, reflect=True)
-print("Reflected tables")
+from datetime import datetime,timedelta
 
 
-# Use MetaData from SQLAlchemy to reflect the tables
-metadata = MetaData(bind=engine)
-metadata.reflect()
-
-# Save reference to the table
-Measurement = Base.classes.measurement
-Station = Base.classes.station
-
-# Create our session (link) from Python to the DB
-session = Session(engine)
-
-#checking data in the measurement table
-table = sqlalchemy.Table('measurement', metadata, autoload=True)
-
-# Test that the insert works by fetching the first 5 rows. 
-conn.execute("select * from measurement limit 5").fetchall()
-
+import climate_analysis as helper
 
 
 app = Flask(__name__)
@@ -42,17 +13,61 @@ app = Flask(__name__)
 @app.route("/")
 def welcome():
     """List all available api routes."""
-    return ("Available Routes:<br/> \ /api/v1.0/precipitation<br/> \ /api/v1.0/stations <br/> \/api/v1.0/start <br/>             /api/v1.0/start/end <br/>" 
-            )
-
-
+    return ("Available Routes:<br/> \
+            /api/v1.0/precipitation<br/> \
+            /api/v1.0/temperature<br>\
+            /api/v1.0/stations<br>\
+            /api/v1.0/start<br>\
+            /api/v1.0/startend")
+            
+            
 #Query for the dates and temperature observations from the last year.
 
 @app.route("/api/v1.0/precipitation")
 def getprecipitationinfo():
-   precipitation_df = precipitation_data()
+   sdate = datetime.now()
+   #print("!!!!!!!!!!!!",sdate)
+   startdate=datetime.date(sdate)
+   precipitation_df = helper.flask_precipitation_data(startdate)
    precipitation_list = precipitation_df.to_dict(orient='index')
    result = jsonify(precipitation_list)
    return result
+	
+@app.route("/api/v1.0/stations")
+def getstationinfo():
+   station_df = helper.station_data()
+   station_list = station_df.to_dict(orient='index')
+   result1 = jsonify(station_list)
+   return result1
 
+@app.route("/api/v1.0/tobs")
+def gettempinfo():
+   sdate = datetime.now()
+   #print("!!!!!!!!!!!!",sdate)
+   startdate=datetime.date(sdate)
+   temp_df = helper.flask_temp_data(startdate)
+   temp_list = temp_df.to_dict(orient='index')
+   result4 = jsonify(temp_list)
+   return result4 
+   
+@app.route("/api/v1.0/start")
+def getstartdateinfo():
+   startdate = '2016-01-01'
+   station_df = helper.flask_start_temps(startdate)
+   station_list = station_df.to_dict(orient='index')
+   result2 = jsonify(station_list)
+   return result2
+   
+@app.route("/api/v1.0/startend")
+def getstartenddateinfo():
+   sdate = datetime.now()
+   startdate=datetime.date(sdate)
+   enddate = startdate - timedelta(days=365)
+   startdate = '2016-01-01'
+   enddate = '2017-01-01'
+   station_df = helper.flask_calc_temps(startdate,enddate)
+   station_list = station_df.to_dict(orient='index')
+   result3 = jsonify(station_list)
+   return result3     
+	
 app.run(port=5000)
--- climate_analysis.py
+++ climate_analysis.py
@@ -1,39 +1,23 @@
 
-# coding: utf-8
-
 # ### Climate Analysis and Exploration
 
 # Import our dependencies
 import pandas as pd
-import os
-from pathlib import Path
-import csv
+import matplotlib
 import matplotlib.pyplot as plt
-import numpy as np
-import datetime
-from flask import Flask, jsonify
 
 # Python SQL toolkit and Object Relational Mapper
-import sqlalchemy
 from sqlalchemy.sql import label
 from sqlalchemy import func
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
-import pandas as pd
 from sqlalchemy import Column, Integer, String, Numeric, Text, Float,Table,ForeignKey
-from flask import jsonify
-
-import matplotlib.pyplot as plt
-from matplotlib import cm
-import matplotlib
-
 from datetime import datetime,timedelta
 
 
 # Use a Session to test the Measurement class
 engine = create_engine("sqlite:///hawaii.sqlite")
-
 
 # Declare a Base using `automap_base()`
 Base = automap_base()
@@ -44,35 +28,26 @@ Base.classes.keys()
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
-
 # create a session
 session = Session(engine)
 my_table = Table('Measurement', Base.metadata,
    Column("station", String, ForeignKey("Station.station")),
    autoload=True,autoload_with=engine)
 
-
 #checking data in the measurement table
 engine.execute('SELECT * FROM measurement LIMIT 10').fetchall()
-
 
 #checking data in the station table
 engine.execute('SELECT * FROM station LIMIT 10').fetchall()
 
-
-
 # Start a session to query the database
 session = Session(engine)
 
-
 engine.execute("select * from measurement limit 5").fetchall()
-
 
 start_date = '2010-01-01'
 
-
 end_date = '2011-01-01'
-
 
 
 # Query measurements table and save the query into results
@@ -85,12 +60,34 @@ measurements_year = session.query(Measurement.date,Measurement.prcp).filter(Meas
 # ### Precipitation Analysis
 
 
+from datetime import datetime, timedelta
+
+
+def flask_precipitation_data(sdate):
+   #Get date for last year
+   futuredate = sdate
+   #print("***********",futuredate)
+   past_year = futuredate - timedelta(days=365)
+   measurements_year = session.query(Measurement.date,Measurement.prcp).filter(Measurement.date > past_year).all()
+   
+   #created dictionary to get the data
+   measure_records = []
+   for measure in measurements_year:
+       measure_records.append(measure._asdict())
+   
+   #dictionary to dataframe
+   measurements_df = pd.DataFrame.from_records(measure_records)
+   measurements_df = measurements_df.set_index('date')
+
+   return measurements_df
+
 
 def precipitation_data():
    #Get date for last year
-   current_time = datetime.now()
-   print("***********",current_time)
-   past_year = current_time - timedelta(days=365)
+   futuredate = datetime.now()
+   #print("***********",futuredate)
+   #current_time = datetime.now()
+   past_year = futuredate - timedelta(days=365)
    measurements_year = session.query(Measurement.date,Measurement.prcp).filter(Measurement.date > past_year).all()
    
    #created dictionary to get the data
@@ -117,20 +114,11 @@ plt.show()
 
 
 # ### Station Analysis
-
-
-
 #Design a query to calculate the total number of stations.
 engine.execute("select * from station").fetchall()
 
-
-
-
 #Creating the query to get active stations
 active_stations = session.query(Station.name,Station.station,label('number_of_obs',func.count(Measurement.id))).                   filter(Measurement.station == Station.station).group_by(Station.name,Station.station).order_by(func.count(Measurement.id).desc())
-
-
-
 
 #Design a query to find the most active stations.
 #List the stations and observation counts in descending order
@@ -148,18 +136,30 @@ def station_data():
    return measurements_df
 
 
-
 station_df=station_data()
 station_df.head()
-
-
 
 #Which station has the highest number of observations?
 station_id = station_df.iloc[:1]['station'][0]
 print("Station with highest number of observations  is : " ,station_id)
 
 
+def flask_temp_data(sdate):
+   #Get date for last year
+   futuredate = sdate
+   past_year = futuredate - timedelta(days=365)
+   measurements_year = session.query(Measurement.date,Measurement.tobs).filter(Measurement.date > past_year).filter(Measurement.station ==station_id).all()
+   
+   #created dictionary to get the data
+   measure_records = []
+   for measure in measurements_year:
+       measure_records.append(measure._asdict())
+   
+   #dictionary to dataframe
+   measurements_df = pd.DataFrame.from_records(measure_records)
+   measurements_df = measurements_df.set_index('date')
 
+   return measurements_df
 
 def temp_data():
    #Get date for last year
@@ -178,14 +178,8 @@ def temp_data():
 
    return measurements_df
 
-
-
-
 tobs_df= temp_data()
 tobs_df.head()
-
-
-
 
 #draw histogram for temperature
 tobs_df.plot.hist(alpha=0.5,bins=12 ,figsize=(10,10))
@@ -198,16 +192,12 @@ plt.show()
 
 # ### Temperature Analysis
 
-
-
-
 #Write a function called `calc_temps` that will accept a start date and end date in the format `%Y-%m-%d` and return the minimum, average, and maximum temperatures for that range of dates.
 #startdate = "2016-01-02"
 #enddate = "2017-01-02"
 
 def calc_temps(startdate,enddate):
   temperature_vacation = session.query(label('max_temp',func.max(Measurement.tobs)),                                   label('min_temp',func.min(Measurement.tobs)),                                   label('avg_temp',func.avg(Measurement.tobs))).                  filter(Measurement.date >= startdate).                  filter(Measurement.date <= enddate )
-
   Max_temp = temperature_vacation[0].max_temp
   Min_temp = temperature_vacation[0].min_temp
   Avg_temp = temperature_vacation[0].avg_temp
@@ -240,3 +230,38 @@ startdate=datetime.datetime.strptime(sdate, "%Y-%m-%d")
 enddate = startdate + datetime.timedelta(days=365)
 calc_temps(startdate,enddate)
 
+def flask_calc_temps(startdate,enddate):
+    temperature_vacation = session.query(label('max_temp',func.max(Measurement.tobs)),label('min_temp',func.min(Measurement.tobs)),label('avg_temp',func.avg(Measurement.tobs))).filter(Measurement.date >= startdate).filter(Measurement.date <= enddate )
+    Max_temp = temperature_vacation[0].max_temp
+    Min_temp = temperature_vacation[0].min_temp
+    Avg_temp = temperature_vacation[0].avg_temp
+    print("Tempture for date range:",Max_temp ,Min_temp,Avg_temp)
+    print("Last year tempature for same date range :Max Temp,MIN temp,Avg temp :", Max_temp,Min_temp,Avg_temp)
+   #created dictionary to get the data
+    measure_records = []
+    for measure in temperature_vacation:
+        measure_records.append(measure._asdict())
+   
+   #dictionary to dataframe
+    measurements_df = pd.DataFrame.from_records(measure_records)
+    measurements_df = measurements_df.set_index('date')
+
+    return measurements_df
+
+def flask_start_temps(startdate):
+    temperature_vacation = session.query(label('max_temp',func.max(Measurement.tobs)),label('min_temp',func.min(Measurement.tobs)),label('avg_temp',func.avg(Measurement.tobs))).filter(Measurement.date >= startdate)
+    Max_temp = temperature_vacation[0].max_temp
+    Min_temp = temperature_vacation[0].min_temp
+    Avg_temp = temperature_vacation[0].avg_temp
+    print("Tempture for date range:",Max_temp ,Min_temp,Avg_temp)
+    print("Last year tempature for same date range :Max Temp,MIN temp,Avg temp :", Max_temp,Min_temp,Avg_temp)
+   #created dictionary to get the data
+    measure_records = []
+    for measure in temperature_vacation:
+        measure_records.append(measure._asdict())
+   
+   #dictionary to dataframe
+    measurements_df = pd.DataFrame.from_records(measure_records)
+    measurements_df = measurements_df.set_index('date')
+
+    return measurements_df
